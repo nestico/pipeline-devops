bf7b1e7e66a99e6b44aa563cae404fcd805f4301
Ernesto Hernandez <gastronomo76@gmail.com>
2022-02-01 18:42:38 -0500

    Lógica de pipelines para ejemplo-gradle2

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -16,7 +16,7 @@ def call(){
   }
   stage("Paso 4: Sonar - Análisis Estático"){
       sh "echo 'Análisis Estático!'"
-      withSonarQubeEnv('sonarqube3') {
+      withSonarQubeEnv('sonarqube') {
           sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
       }
   }
@@ -25,7 +25,7 @@ def call(){
       sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
   }
   stage("Paso 6: Subir Nexus"){
-      nexusPublisher nexusInstanceId: 'nexus3',
+      nexusPublisher nexusInstanceId: 'nexus',
       nexusRepositoryId: 'devops-usach-nexus',
       packages: [
           [$class: 'MavenPackage',
@@ -45,7 +45,7 @@ def call(){
       ]
   }
   stage("Paso 7: Descargar Nexus"){
-      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+      sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
   }
   stage("Paso 8: Levantar Artefacto Jar"){
       sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
